b518414e084be3ce42366932c53cec095f523921
loicruterana <lruterana@gmail.com>
2023-05-10 10:24:08 +0200

    Test Page Film Complétée

--- server.js
+++ server.js
@@ -8,9 +8,12 @@ const port = 4000;
 async function getMovieDetails(req, res) {
   
   let data = []
+
+      const filmID = 78;
+
       try {
-          const response = await fetch(`https://api.themoviedb.org/3/movie/346?api_key=82184d982ef42b5548f45d546dd62ddb&language=fr-FR`)
-          // const response = await fetch(`https://api.themoviedb.org/3/movie/${movie.id}?api_key=82184d982ef42b5548f45d546dd62ddb&language=fr-FR`)
+          // const response = await fetch(`https://api.themoviedb.org/3/movie/134374?api_key=82184d982ef42b5548f45d546dd62ddb&language=fr-FR`)
+          const response = await fetch(`https://api.themoviedb.org/3/movie/${filmID}?api_key=82184d982ef42b5548f45d546dd62ddb&language=fr-FR`)
           const responseData = await response.json()
           data = responseData
 
@@ -29,10 +32,6 @@ getMovieDetails();
 // View engine setup
 app.set('view engine', 'ejs');
 
-// app.get('/', (req, res) => {
-//   // console.log('show Home');
-//   res.render('testHome');
-// });
 
 app.get('/', getMovieDetails);
 
