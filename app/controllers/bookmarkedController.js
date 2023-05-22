13a2230db7b443f2534805e96643c7f5be00b680
loicruterana <lruterana@gmail.com>
2023-05-22 09:57:17 +0200

    <feat>(<kinomatch>):commit systeme de bookmark

--- app/controllers/bookmarkedController.js
+++ app/controllers/bookmarkedController.js
@@ -22,10 +22,11 @@ const bookmarkedController = {
     addBookmarkedMovie: async (req, res) => {
 
         const UserID = '3';
-        const movieID = '376';
+        const movieID = '335';
 
         // const UserID = req.session.user;
         // const UserID = req.body.UserID;
+
         // const movieID = req.body.MovieID;
 
 
