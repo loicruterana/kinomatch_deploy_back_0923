185150d5e0b44b0a854c288bc3a4dbab792efbcb
loicruterana <lruterana@gmail.com>
2023-05-23 14:33:34 +0200

    <fix>(<kinomatch>):commit correction decade

--- app/utils/TMDB.js
+++ app/utils/TMDB.js
@@ -46,7 +46,7 @@ const TMDB = {
         let urlGenre = '';
 
 
-        if(year1 & year2 !== NaN){
+        if(!isNaN(year1)){
             urlDecade = `&primary_release_date.gte=${year1}&primary_release_date.lte=${year2}`; 
         }
 
