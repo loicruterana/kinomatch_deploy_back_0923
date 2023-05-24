9427bf26eac495a9c7fba46ca2dc7b5fd2e7268f
loicruterana <lruterana@gmail.com>
2023-05-24 13:45:59 +0200

    <feat>(<kinomatch>):commit differentiate movie providers

--- app/controllers/bookmarkedController.js
+++ app/controllers/bookmarkedController.js
@@ -8,7 +8,7 @@ const bookmarkedController = {
 
     bookmarkedList: async (req, res) => {
 
-        const UserID = req.body.UserID;
+        const UserID = '4';
         // const UserID = req.session.user;
 
         const bookmarkedList = await Bookmarked.findAll({
