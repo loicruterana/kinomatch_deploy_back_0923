c478aa9e0d5c6671c15e12d7da521c3df9d54bea
loicruterana <lruterana@gmail.com>
2023-05-25 12:27:52 +0200

    <feat>(<kinomatch>):commit add bookmark to db

--- app.js
+++ app.js
@@ -27,8 +27,12 @@ app.use(express.json());
 
 // Middleware to set the Access-Control-Allow-Origin header
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
-  next();
+  res.header('Access-Control-Allow-Origin', '*'); 
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+next();
 });
 
 // j'ajoute le middleware d'express session, qu'on configure
