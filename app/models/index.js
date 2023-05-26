c17698cefa11478546b2da5e6f0bebdc836c125c
loicruterana <lruterana@gmail.com>
2023-05-26 09:58:28 +0200

    <feat>(<kinomatch>):commit reset bdd

--- app/models/index.js
+++ app/models/index.js
@@ -1,6 +1,9 @@
 const Film = require('./film');
 const User = require('./user');
 const Bookmarked = require('./bookmarked');
+const ToWatch = require('./toWatch');
+const Watched = require('./watched');
+
 
 
 
